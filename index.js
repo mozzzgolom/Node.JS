99472ac1cbfc2d00aa10d51e7ac96ac95a636f38
mozzzgolom <mozzzgolom@gmal.com>
2021-10-21 09:16:09 +0300

    дз4

--- index.js
+++ index.js
@@ -1,30 +1,68 @@
 const fs = require('fs')
 const lazy = require('lazy')
+const path = require('path')
+const yargs = require('yargs')
+const readline = require("readline");
+const inquirer = require("inquirer");
 
-const readStream = fs.createReadStream('./access.log', {
-    encoding: 'utf-8',
-})
+const options = yargs
+    .usage("Usage: -p <path>")
+    .option("p", { alias: "path", describe: "Path to file", type: "string", demandOption: true })
+    .argv;
 
-const writeStream = fs.createWriteStream(`./89.123.1.41_requests.log`, {
-    encoding:'utf8',
-    flags: 'a'
-});
-const writeStream2 = fs.createWriteStream(`./34.48.240.111_requests.log`, {
-    encoding:'utf8',
-    flags: 'a'
-});
+const filePath = path.join(__dirname, options.path);
 
-new lazy(readStream)
-    .lines
-    .forEach(function(line) {
-        let result = String(line).match(/89.123.1.41/)
-        let result2 = String(line).match(/34.48.240.111/)
-        if(result !== null) {
-            writeStream.write(line)
-            writeStream.write('\n')
-        }
-        if(result2 !== null) {
-            writeStream2.write(line)
-            writeStream2.write('\n')
-        }
-    })
+const isFile = fileName => {
+    return fs.lstatSync(fileName).isFile();
+}
+
+// const question = async (query) => rl.question(query, resolve => resolve) // new Promise(resolve => rl.question(query, resolve) )
+
+const readingFile = (filePath) => {
+    if(isFile(filePath)) {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        rl.question("Введите шаблон поиска: ", (template) => {
+            console.log(template, options.path, filePath)
+            const readStream = fs.createReadStream(filePath, {
+                encoding: 'utf-8',
+            })
+
+            const writeStream = fs.createWriteStream(`${options.path}_${template}_requests.log`, {
+                encoding:'utf8',
+                flags: 'a'
+            });
+
+            new lazy(readStream)
+                .lines
+                .forEach(function(line) {
+                    let result = String(line).match(template)
+                    if(result !== null) {
+                        writeStream.write(line)
+                        writeStream.write('\n')
+                    }
+                })
+
+            rl.close();
+        });
+
+    } else {
+        console.log('no')
+        const list = fs.readdirSync(filePath)
+        inquirer
+            .prompt([{
+                name: "fileName",
+                type: "list",
+                message: "Choose file:",
+                choices: list,
+            }])
+            .then((answer) => {
+                console.log(answer.fileName);
+                readingFile(filePath + '/' + answer.fileName)
+            });
+    }
+}
+readingFile(filePath)
