560fe9a4a85315af318cf819d0f38a37b5b9c5dc
mozzzgolom <mozzzgolom@gmal.com>
2021-10-21 09:13:55 +0300

    дз3

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-const readline = require('readline');
-const EventEmitter = require('events');
-const Timer = require('./timer')
+const fs = require('fs')
+const lazy = require('lazy')
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+const readStream = fs.createReadStream('./access.log', {
+    encoding: 'utf-8',
+})
+
+const writeStream = fs.createWriteStream(`./89.123.1.41_requests.log`, {
+    encoding:'utf8',
+    flags: 'a'
+});
+const writeStream2 = fs.createWriteStream(`./34.48.240.111_requests.log`, {
+    encoding:'utf8',
+    flags: 'a'
 });
 
-class Handler {
-    static timer(payload ) {
-        Timer.timer(payload)
-    }
-}
-
-class MyEmitter extends EventEmitter {};
-const emitterObject = new MyEmitter();
-emitterObject.on('timer', Handler.timer);
-
-rl.question('Введите дату в формате "минуты-час-день-месяц-год" или продолжите для установки таймера на минуту: ',
-    (date) => {
-    emitterObject.emit('timer', date);
-    rl.close();
-})
+new lazy(readStream)
+    .lines
+    .forEach(function(line) {
+        let result = String(line).match(/89.123.1.41/)
+        let result2 = String(line).match(/34.48.240.111/)
+        if(result !== null) {
+            writeStream.write(line)
+            writeStream.write('\n')
+        }
+        if(result2 !== null) {
+            writeStream2.write(line)
+            writeStream2.write('\n')
+        }
+    })
